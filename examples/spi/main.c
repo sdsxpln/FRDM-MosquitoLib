1271e321dcda2443e0ed006cbabd1c321d7b51b8
roel0 <roel.postelmans@altran.com>
2015-12-13 17:11:35 +0100

    Weekend work

---
+++ examples/spi/main.c
@@ -0,0 +1,40 @@
+#include "MK64F12.h"
+#include "spi.h"
+
+
+#define led_blue_init() { PORTB_PCR21 = PORT_PCR_MUX(1); GPIOB_PDDR |= (1 << 21); }
+#define led_blue_toogle() { GPIOB_PTOR |= (1 << 21); }
+
+
+#define WRITE_SPI_CMD0(byte) SPI0_PUSHR = (byte | SPI_PUSHR_CONT | ulChipSelectLine | SPI_PUSHR_CTAS_CTAR0)
+
+int main(int argc, char *argv[]) {
+  SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;    /*Enable Port B Clock Gate Control*/
+  SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;    /*Enable Port E Clock Gate Control*/
+  SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;    /*Enable Port C Clock Gate Control*/
+  SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;    /*Enable Port A Clock Gate Control*/
+
+	led_blue_init();
+	led_blue_toogle();
+  
+  struct frdm_spi_mode mode;
+  mode.mode = FRDM_SPI_MASTER;
+  mode.CPOL = FRDM_SPI_CPOL_RISING_EDGE;
+  mode.CPHA = FRDM_SPI_CPHA_FIRST_EDGE;
+  mode.frame = 8;
+
+  frdm_spi_init(SPI0,&mode,0);
+
+  while(1) {
+	led_blue_toogle();
+    	delay(2000);
+        WRITE_SPI_CMD0(0x99);
+  }
+  return 0;
+}
+
+void delay(int time)
+{
+	int i = 0;
+	for (i = time*1000 ; i !=0; i--) { }
+}
