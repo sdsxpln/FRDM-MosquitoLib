8ccf26a6e25608b343cd712c9fb458f29cc20575
roel0 <roel.postelmans@altran.com>
2015-12-17 08:19:26 +0100

    Added port multiplexing for spi0. Continious clock is now visible on scope

--- examples/spi/main.c
+++ examples/spi/main.c
@@ -12,7 +12,14 @@ int main(int argc, char *argv[]) {
   SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;    /*Enable Port E Clock Gate Control*/
   SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;    /*Enable Port C Clock Gate Control*/
   SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;    /*Enable Port A Clock Gate Control*/
+  SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;    /*Enable Port D Clock Gate Control*/
+  SIM_SCGC6 |= SIM_SCGC6_SPI0_MASK;
 
+  //PORT D
+  PORTD_PCR(0) = PORT_PCR_MUX(2); //PCS0
+  PORTD_PCR(1) = PORT_PCR_MUX(2); //SCK
+  PORTD_PCR(2) = PORT_PCR_MUX(2); //SOUT
+  PORTD_PCR(3) = PORT_PCR_MUX(2); //SIN
 	led_blue_init();
 	led_blue_toogle();
   
