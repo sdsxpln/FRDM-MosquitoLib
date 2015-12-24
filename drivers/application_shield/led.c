79a54dab2736bbc6cc4a55d2857794c06ad54e7a
roel0 <roel.postelmans@altran.com>
2015-12-24 08:21:11 +0100

    added blink func

--- drivers/application_shield/led.c
+++ drivers/application_shield/led.c
@@ -1,29 +1,53 @@
 #include "MK64F12.h"
 
+/*******************************************************
+ * Led module
+ * The led is routed as followed:
+ * R       PTA0
+ * G       PTC4
+ * B       PTA2
+ *
+ * FlexTimers with PWM functionallity are used to controll
+ * the intensity of the colors
+ **********************************************************/
+
+static inline void RGB_LED(uint32_t red, uint32_t green, uint32_t blue) {
+    //FTM0_C5V = red;
+    FTM0_C4V = green;
+    FTM0_C7V = blue;
+}
+
 void frdm_as_led_init() {
     // Power on
     SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK | SIM_SCGC5_PORTA_MASK;
     SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
-    SIM_SOPT2 |= SIM_SOPT2_FTM0SRC(1);
+    //SIM_SOPT4 |= SIM_SOPT4_FTM0TRG0SRC(1);
 
     //NOTE: multiplexing port pat0 will cause cpu to hard fault on revision other then E
     // because it's also routed to the debugger
     //PORTA_PCR0 = PORT_PCR_MUX(3);  // FTM0_CH5
-    PORTA_PCR2 = PORT_PCR_MUX(3);  // FTM0_CH7
     PORTC_PCR4 = PORT_PCR_MUX(4);  // FTM0_CH4
+    PORTA_PCR2 = PORT_PCR_MUX(3);  // FTM0_CH7
 
+    RGB_LED(0,0,0);
+    // Make write_protected registers writable
+    FTM0_MODE |= FTM_MODE_WPDIS_MASK;
+    FTM0_MODE &= ~ FTM_MODE_FTMEN_MASK;
+    // Overflow at 99
     FTM0_MOD  = 99;
-    FTM0_C4SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
+    FTM0_CNTIN = 0;
+    FTM0_CNT = 0;
+    FTM0_C4SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK | FTM_CnSC_ELSB_MASK;
     //FTM0_C5SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
-    FTM0_C7SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
-    FTM0_SC   = FTM_SC_CMOD(1) | FTM_SC_PS(0);     /* Edge Aligned PWM running from BUSCLK / 1 */
+    FTM0_C7SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK | FTM_CnSC_ELSB_MASK;
+    FTM0_SC   = FTM_SC_CLKS(1) | FTM_SC_PS(7);     /* Edge Aligned PWM running from BUSCLK / 128 */
+    RGB_LED(400,400,0);
 }
 
-void frdm_as_led_toggle_R() {
+// Blink an LED based on a pattern bitmask
+void frdm_as_led_blink_G(uint32_t *pattern) {
+    RGB_LED(0,*pattern & 1 ? 100 : 0, 0);           // Set RED led based on LSB
+    *pattern = (*pattern >> 1) | (*pattern << 31);
 }
 
-void frdm_as_led_toggle_G() {
-}
 
-void frdm_as_led_toggle_B() {
-}
