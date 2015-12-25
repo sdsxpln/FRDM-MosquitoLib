feed42f54639ac1457eaa088cd390a2732bc2e80
roel0 <roel.postelmans@altran.com>
2015-12-25 09:43:26 +0100

    Blue and Red were switched

--- drivers/application_shield/led.c
+++ drivers/application_shield/led.c
@@ -3,9 +3,9 @@
 /*******************************************************
  * Led module
  * The led is routed as followed:
- * R       PTA0
+ * R       PTA2
  * G       PTC4
- * B       PTA2
+ * B       PTA0
  *
  * FlexTimers with PWM functionallity are used to controll
  * the intensity of the colors
@@ -15,9 +15,9 @@ static uint32_t _G = 0;
 static uint32_t _B = 0;
 
 static inline void RGB_LED(uint32_t red, uint32_t green, uint32_t blue) {
-    //FTM0_C5V = red;
+    FTM0_C7V = red;
     FTM0_C3V = green;
-    FTM0_C7V = blue;
+    //FTM0_C5V = blue;
     _R = red;
     _G = green;
     _B = blue;
@@ -29,11 +29,11 @@ void frdm_as_led_init() {
     SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
     SIM_SOPT4 |= SIM_SOPT4_FTM0TRG0SRC(1);
 
-    //NOTE: multiplexing port pat0 will cause cpu to hard fault on revision other then E
+    //NOTE: multiplexing port pta0 will cause cpu to hard fault on revision other then E
     // because it's also routed to the debugger
-    //PORTA_PCR0 = PORT_PCR_MUX(3);  // FTM0_CH5
-    PORTC_PCR4 = PORT_PCR_MUX(4);  // FTM0_CH3
     PORTA_PCR2 = PORT_PCR_MUX(3);  // FTM0_CH7
+    PORTC_PCR4 = PORT_PCR_MUX(4);  // FTM0_CH3
+    //PORTA_PCR0 = PORT_PCR_MUX(3);  // FTM0_CH5
 
     RGB_LED(0,0,0);
     // Make write_protected registers writable
@@ -44,7 +44,7 @@ void frdm_as_led_init() {
     FTM0_CNTIN = 0;
     FTM0_CNT = 0;
     FTM0_C3SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK | FTM_CnSC_ELSB_MASK;
-    //FTM0_C5SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
+    FTM0_C5SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
     FTM0_C7SC = FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK | FTM_CnSC_ELSB_MASK;
     FTM0_SC   = FTM_SC_CLKS(1) | FTM_SC_PS(7);     /* Edge Aligned PWM running from BUSCLK / 128 */
 }
