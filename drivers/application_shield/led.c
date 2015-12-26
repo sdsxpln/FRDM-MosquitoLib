e284c2faf687a79dce3169d9526ec996f88136cd
roel0 <roel.postelmans@altran.com>
2015-12-26 09:35:49 +0100

    Added intensity argument

--- drivers/application_shield/led.c
+++ drivers/application_shield/led.c
@@ -1,5 +1,3 @@
-#include "MK64F12.h"
-
 /*******************************************************
  * Led module
  * The led is routed as followed:
@@ -9,12 +7,24 @@
  *
  * FlexTimers with PWM functionallity are used to controll
  * the intensity of the colors
+ *
+ * @ known issues: PTA0 (blue) is also routed for some debugger
+ *                 functionallity on board revisions other than E.
+ *                 Multiplexing this port as PTA0 while debugging,
+ *                 will cause the CPU to hard fault.
+ *
+ *                 In order to prevent some hard to find bugs
+ *                 , the blue led is currently
+ *                 unsupported
  **********************************************************/
-static uint32_t _R = 0;
-static uint32_t _G = 0;
-static uint32_t _B = 0;
+#include "MK64F12.h"
+#include "led.h"
 
-static inline void RGB_LED(uint32_t red, uint32_t green, uint32_t blue) {
+static uint8_t _R = 0;
+static uint8_t _G = 0;
+static uint8_t _B = 0;
+
+static inline void RGB_LED(uint8_t red, uint8_t green, uint8_t blue) {
     FTM0_C7V = red;
     FTM0_C3V = green;
     //FTM0_C5V = blue;
@@ -29,8 +39,6 @@ void frdm_as_led_init() {
     SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
     SIM_SOPT4 |= SIM_SOPT4_FTM0TRG0SRC(1);
 
-    //NOTE: multiplexing port pta0 will cause cpu to hard fault on revision other than E
-    // because it's also routed to the debugge, on normal run it works finer
     PORTA_PCR2 = PORT_PCR_MUX(3);  // FTM0_CH7
     PORTC_PCR4 = PORT_PCR_MUX(4);  // FTM0_CH3
     //PORTA_PCR0 = PORT_PCR_MUX(3);  // FTM0_CH5
@@ -49,12 +57,12 @@ void frdm_as_led_init() {
     FTM0_SC   = FTM_SC_CLKS(1) | FTM_SC_PS(7);     /* Edge Aligned PWM running from BUSCLK / 128 */
 }
 
-void frdm_as_led_toggle_G() {
-    RGB_LED(_R,_G>0 ? 0 : 50, _B);
+void frdm_as_led_toggle_G(uint8_t intensity) {
+    RGB_LED(_R,_G>0 ? 0 : intensity, _B);
 }
-void frdm_as_led_toggle_R() {
-    RGB_LED(_R>0 ? 0 : 50, _G, _B);
+void frdm_as_led_toggle_R(uint8_t intensity) {
+    RGB_LED(_R>0 ? 0 : intensity, _G, _B);
 }
-void frdm_as_led_toggle_B() {
-    RGB_LED(_R,_G,_B>0 ? 0 : 50);
+void frdm_as_led_toggle_B(uint8_t intensity) {
+    RGB_LED(_R,_G,_B>0 ? 0 : intensity);
 }
