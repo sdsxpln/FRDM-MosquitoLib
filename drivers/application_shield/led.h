a0e6fb25410a46bbb499957e8488537bbc8a79c7
roel0 <roel.postelmans@altran.com>
2015-12-26 09:36:15 +0100

    Header creation

---
+++ drivers/application_shield/led.h
@@ -0,0 +1,13 @@
+#ifndef _LED_H_
+#define _LED_H_
+
+#define FRDM_AS_LED_INT_NORMAL  50
+#define FRDM_AS_LED_INT_HIGH   100
+
+extern void frdm_as_led_init();
+
+extern void frdm_as_led_toggle_R(uint8_t intensity);
+extern void frdm_as_led_toggle_G(uint8_t intensity);
+extern void frdm_as_led_toggle_B(uint8_t intensity);
+
+#endif
