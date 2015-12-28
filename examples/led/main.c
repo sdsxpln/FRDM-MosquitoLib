a960c28f387f4ad00865703d8483ea9718e934be
roel0 <roel.postelmans@altran.com>
2015-12-28 15:21:42 +0100

    Extended example for all colors

--- examples/led/main.c
+++ examples/led/main.c
@@ -1,8 +1,6 @@
 #include "MK64F12.h"
+#include "as_led.h"
 #include "led.h"
-
-#define led_blue_init() { SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; PORTB_PCR21 = PORT_PCR_MUX(1); GPIOB_PDDR |= (1 << 21); }
-#define led_blue_toogle() { GPIOB_PTOR |= (1 << 21); }
 
 void delay(int time);
 
@@ -10,15 +8,25 @@ int main(void)
 {
   uint32_t pattern = 0b10101101;
   frdm_as_led_init();
-  led_blue_init();
-  led_blue_toogle();
+  frdm_led_init();
 
   for (;;) {
-    //Blink led on board
-    led_blue_toogle();
-    // Blink red led on application shield
+    //Blink led on board and application shield
+    frdm_led_toggle_G();
+    frdm_as_led_toggle_G(FRDM_AS_LED_INT_NORMAL);
+    delay(2000);
+    frdm_led_toggle_G();
+    frdm_led_toggle_B();
+    frdm_as_led_toggle_G(FRDM_AS_LED_INT_NORMAL);
+    frdm_as_led_toggle_B(FRDM_AS_LED_INT_NORMAL);
+    delay(2000);
+    frdm_led_toggle_B();
+    frdm_led_toggle_R();
+    frdm_as_led_toggle_B(FRDM_AS_LED_INT_NORMAL);
     frdm_as_led_toggle_R(FRDM_AS_LED_INT_NORMAL);
     delay(2000);
+    frdm_led_toggle_R();
+    frdm_as_led_toggle_R(FRDM_AS_LED_INT_NORMAL);
   }
   return 0;
 }
