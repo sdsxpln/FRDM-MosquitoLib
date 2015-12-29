e091205bc8362fec9d355bf43dfcd4bea96fa5e1
roel0 <roel.postelmans@altran.com>
2015-12-29 08:35:32 +0100

    Added all lcd macro's

--- drivers/application_shield/lcd_C12832A1Z.c
+++ drivers/application_shield/lcd_C12832A1Z.c
@@ -1,10 +1,34 @@
 #include "MK64F12.h"
 #include "spi.h"
 
+#define LCD_DISPLAY_STATE   0xAE
+#define LCD_DISPLAY_OFF     (LCD_DISPLAY_STATE & 0)
+#define LCD_DISPLAY_ON      (LCD_DISPLAY_STATE & 1)
+#define LCD_LINE_ZERO       0x40
+#define LCD_LINE(x)         (LCD_LINE_ZERO & x)
+#define LCD_ROW_MSB(x)      (0x08 & (x & 0x07))
+#define LCD_ROW_LSB(x)      (0x00 & (x & 0x07))
+#define LCD_ACD_NORMAL      0xA0
+#define LCD_ACD_REVERSE     (LCD_ACD_NORMAL & 1)
+#define LCD_DISPLAY_NORMAL  0xA6
+#define LCD_DISPLAY_REVERSE (LCD_DISPLAY_NORMAL & 1)
+#define LCD_ALL_POINTS      0xA4
+#define LCD_ALL_POINTS_OFF  (LCD_ALL_POINTS & 0)
+#define LCD_ALL_POINTS_ON   (LCD_ALL_POINTS & 1)
+#define LCD_BIAS_9          0xA2
+#define LCD_BIAS_7          (LCD_BIAS_9 & 1)
+#define LCD_MODIFY          0xE0
+#define LCD_END             0xEE
+#define LCD_RESET           0xE2
+#define LCD_NOP             0xE3
+#define LCD_POWER_ON        0x2F
+#define LCD_VOLTAGE_RATIO   0x22
+#define LCD_OUTPUT_NORMAL   0xC8
+#define LCD_SET_CONTRAST(x) frdm_lcd_out(0x81); frdm_lcd_out(x);
 
 void frdm_lcd_out(uint8_t data)
 {
-  GPIOC_PTOR &= (0<<3); //A0  = 0;
+  GPIOC_PTOR |= (0<<3); //A0  = 0;
   frdm_spi_master_write_byte(SPI0, data);
 }
 
@@ -19,21 +43,19 @@ void init_LCD()
   mode.frame = 8;
   frdm_spi_init(SPI0, &mode, 20000000);
 
-  frdm_lcd_out(0xAE);   //  display off
-  frdm_lcd_out(0xA2);   //  bias voltage
+  frdm_lcd_out(LCD_DISPLAY_OFF);   //  display off
+  frdm_lcd_out(LCD_BIAS_9);   //  bias voltage
 
-  frdm_lcd_out(0xA0);   //  ADC select
-  frdm_lcd_out(0xC8);   //  colum normal
+  frdm_lcd_out(LCD_ACD_NORMAL);   //  ADC select
+  frdm_lcd_out(LCD_OUTPUT_NORMAL);   //  colum normal
 
-  frdm_lcd_out(0x22);   //  voltage resistor ratio
-  frdm_lcd_out(0x2F);   //  power on
-  //frdm_lcd_out(0xA4);   //  LCD display ram
-  frdm_lcd_out(0x40);   // start line = 0
-  frdm_lcd_out(0xAF);     // display ON
+  frdm_lcd_out(LCD_VOLTAGE_RATIO);   //  voltage resistor ratio
+  frdm_lcd_out(LCD_POWER_ON);   //  power on
+  frdm_lcd_out(LCD_LINE_ZERO);   // start line = 0
+  frdm_lcd_out(LCD_DISPLAY_ON);     // display ON
 
-  frdm_lcd_out(0x81);   //  set contrast
-  frdm_lcd_out(0x17);   //  set contrast
+  LCD_SET_CONTRAST(0x17);
 
-  frdm_lcd_out(0xA6);     // display normal
+  frdm_lcd_out(LCD_DISPLAY_NORMAL);     // display normal
 }
 
