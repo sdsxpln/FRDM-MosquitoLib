878d6ea886f3e8016683516b60fc5d89c7a52b04
roel0 <roel.postelmans@altran.com>
2015-12-30 09:07:52 +0100

    Added lcd update func, extend initialisation of the lcd

--- drivers/application_shield/lcd_C12832A1Z.c
+++ drivers/application_shield/lcd_C12832A1Z.c
@@ -6,7 +6,7 @@
 #define LCD_DISPLAY_ON      (LCD_DISPLAY_STATE & 1)
 #define LCD_LINE_ZERO       0x40
 #define LCD_LINE(x)         (LCD_LINE_ZERO & x)
-#define LCD_ROW_MSB(x)      (0x08 & (x & 0x07))
+#define LCD_ROW_MSB(x)      (0x10 & (x & 0x07))
 #define LCD_ROW_LSB(x)      (0x00 & (x & 0x07))
 #define LCD_ACD_NORMAL      0xA0
 #define LCD_ACD_REVERSE     (LCD_ACD_NORMAL & 1)
@@ -24,18 +24,83 @@
 #define LCD_POWER_ON        0x2F
 #define LCD_VOLTAGE_RATIO   0x22
 #define LCD_OUTPUT_NORMAL   0xC8
-#define LCD_SET_CONTRAST(x) frdm_lcd_out(0x81); frdm_lcd_out(x);
+#define LCD_SET_CONTRAST(x) frdm_as_lcd_out(0x81); frdm_as_lcd_out(x);
+#define LCD_PAGE(x)         (0xB0 & x)
+#define LCD_BUFFER_SIZE     512
+#define LCD_TOTAL_PAGES     4
+static uint8_t buffer[LCD_BUFFER_SIZE];
 
-void frdm_lcd_out(uint8_t data)
+void frdm_as_lcd_out(uint8_t cmd)
 {
-  GPIOC_PTOR |= (0<<3); //A0  = 0;
-  frdm_spi_master_write_byte(SPI0, data);
+  GPIOC_PCOR |= (1<<3); //A0  = 0
+  GPIOD_PCOR |= (1<<0); //CS  = 0
+  frdm_spi_master_write_byte(SPI0, cmd);
+  GPIOD_PSOR |= (1<<0); //CS  = 1
 }
+void frdm_as_lcd_out_data(uint8_t data) {
+  GPIOC_PSOR |= (1<<3); //A0  = 1
+  GPIOD_PCOR |= (1<<0); //CS  = 0
+  frdm_spi_master_write_byte(SPI0, data);
+  GPIOD_PSOR |= (1<<0); //CS  = 1
+}
+/** TODO move to other module **/
+void wait_ms(uint32_t ms) {
+  volatile uint32_t i;
+  for(i=0; i<120000*ms; i++) {
+    // do nothing
+  }
+}
+static void frdm_as_lcd_update(void) {
+  uint16_t i=0,j=0;;
+  uint16_t page_size = LCD_BUFFER_SIZE/LCD_TOTAL_PAGES;
+  //page 0
+  frdm_as_lcd_out(LCD_ROW_LSB(0));
+  frdm_as_lcd_out(LCD_ROW_MSB(0));
+  frdm_as_lcd_out(LCD_PAGE(0));
+  GPIOC_PSOR |= (1<<3); //A0  = 1
+  j += page_size;
+  for(i=0; i<j; i++) {
+    frdm_as_lcd_out_data(buffer[i]);
+  }
 
-void init_LCD()
+  // page 1
+  frdm_as_lcd_out(LCD_ROW_LSB(0));
+  frdm_as_lcd_out(LCD_ROW_MSB(0));
+  frdm_as_lcd_out(LCD_PAGE(1));
+  GPIOC_PSOR |= (1<<3); //A0  = 1
+  j += page_size;
+  for(; i<j; i++) {
+    frdm_as_lcd_out_data(buffer[i]);
+  }
+  //page 2
+  frdm_as_lcd_out(LCD_ROW_LSB(0));
+  frdm_as_lcd_out(LCD_ROW_MSB(0));
+  frdm_as_lcd_out(LCD_PAGE(2));
+  GPIOC_PSOR |= (1<<3); //A0  = 1
+  j += page_size;
+  for(; i<j; i++) {
+    frdm_as_lcd_out_data(buffer[i]);
+  }
+
+  //page 3
+  frdm_as_lcd_out(LCD_ROW_LSB(0));
+  frdm_as_lcd_out(LCD_ROW_MSB(0));
+  frdm_as_lcd_out(LCD_PAGE(3));
+  GPIOC_PSOR |= (1<<3); //A0  = 1
+  GPIOD_PCOR |= (1<<0); //CS  = 0
+  j += page_size;
+  for(; i<j; i++) {
+    frdm_as_lcd_out_data(buffer[i]);
+  }
+}
+void frdm_as_lcd_init()
 {
   PORTC_PCR3 = PORT_PCR_MUX(1);
+  PORTD_PCR0 = PORT_PCR_MUX(1);
   GPIOC_PDDR |= (1 << 3);
+  GPIOD_PDDR |= (1 << 0);
+
+  memset(buffer, 0 , LCD_BUFFER_SIZE);
   struct frdm_spi_mode mode;
   mode.mode = FRDM_SPI_MASTER;
   mode.CPOL = FRDM_SPI_CPOL_FALLING_EDGE;
@@ -43,19 +108,30 @@ void init_LCD()
   mode.frame = 8;
   frdm_spi_init(SPI0, &mode, 20000000);
 
-  frdm_lcd_out(LCD_DISPLAY_OFF);   //  display off
-  frdm_lcd_out(LCD_BIAS_9);   //  bias voltage
+  // Overwrite spi port , MISO is used as reset line
+  PORTD_PCR3 = PORT_PCR_MUX(1);
+  GPIOD_PDDR |= (1 << 3);
 
-  frdm_lcd_out(LCD_ACD_NORMAL);   //  ADC select
-  frdm_lcd_out(LCD_OUTPUT_NORMAL);   //  colum normal
+  GPIOC_PCOR |= (1<<3); //A0  = 0
+  GPIOD_PSOR |= (1<<0); //CS  = 1
+  GPIOD_PSOR |= (1<<3); //R   = 1
+  wait_ms(2);
+  GPIOD_PCOR |= (1<<3); //R   = 1
+  frdm_as_lcd_out(LCD_DISPLAY_OFF);
+  frdm_as_lcd_out(LCD_BIAS_9);
 
-  frdm_lcd_out(LCD_VOLTAGE_RATIO);   //  voltage resistor ratio
-  frdm_lcd_out(LCD_POWER_ON);   //  power on
-  frdm_lcd_out(LCD_LINE_ZERO);   // start line = 0
-  frdm_lcd_out(LCD_DISPLAY_ON);     // display ON
+  frdm_as_lcd_out(LCD_ACD_NORMAL);
+  frdm_as_lcd_out(LCD_OUTPUT_NORMAL);
+
+  frdm_as_lcd_out(LCD_VOLTAGE_RATIO);
+  frdm_as_lcd_out(LCD_POWER_ON);
+  frdm_as_lcd_out(LCD_LINE_ZERO);
+  frdm_as_lcd_out(LCD_DISPLAY_ON);
 
   LCD_SET_CONTRAST(0x17);
 
-  frdm_lcd_out(LCD_DISPLAY_NORMAL);     // display normal
+  frdm_as_lcd_out(LCD_DISPLAY_NORMAL);
+
+  frdm_as_lcd_update();
 }
 
